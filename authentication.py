a5244bfe8ea39cdfc43f972ef9cc6105a214a1ff
Sally-Yang-Jing-Ou <sally_auyoung@hotmail.com>
2015-02-21 18:07:26 -0500

    first 20 tweets GET

--- authentication.py
+++ authentication.py
@@ -1,10 +1,16 @@
 import tweepy
 
 auth = tweepy.OAuthHandler("lgHnhvQExLozd24dliMg1jAlG", "iesymLrCGhL8HcTLqyo0KSmYvZrxcfolqmi1Q06lnj5beHQEcU")
-auth.set_access_token("3050210471-FnVA5ACLtsDoaGqQeDcRuAB0ps05MipZNIP3cEI", "9KCNgCxcvyHgPKbPoUgjOzEAl9BuiOeSgYn7O4RWXekgb")
+auth.set_access_token("3050210471-Ka1iB255E90jNpOVFLFIymGwqXVdQKEw8YgpNlk", "I67LL9hhAmcWobFD8qPaAY7YEKs2gdHlaL0IoOx3ggSmw")
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print tweet.text
+# public_tweets = api.home_timeline()
+# for tweet in public_tweets:
+#     print tweet.text
+
+mentions = api.mentions_timeline(count=20)
+
+for mention in mentions:
+    print mention.text
+    print mention.user.screen_name
